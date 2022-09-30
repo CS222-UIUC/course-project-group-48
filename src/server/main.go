e9f95a18c55dfac5af960a9e6c4ccec2f0b4cbe2
Aryan-V <aryangv2@illinois.edu>
2022-09-29 20:37:39 -0500

    removed unnecessary routes

--- src/server/main.go
+++ src/server/main.go
@@ -15,8 +15,6 @@ func main() {
 	version1_str := "/v1"
 	e.POST(fmt.Sprintf("%s/adduser", version1_str), handlers.Add_user)
 	e.POST(fmt.Sprintf("%s/authuser", version1_str), handlers.Auth_user)
-	// e.POST(fmt.Sprintf("%s/update_record", version1_str), handlers_v1.Update_record)
-	// e.GET(fmt.Sprintf("%s/access_record", version1_str), handlers_v1.Access_record)
 
 	e.Logger.Fatal(e.Start("localhost:1323"))
 }
