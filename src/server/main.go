4f1009196b639ac25ba2b0cdcc4b08cc1eda5918
Aryan-V <aryangv2@illinois.edu>
2022-10-06 22:56:10 -0500

    added user

--- src/server/main.go
+++ src/server/main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 
 	handlers "server/apihandlers"
 
 	"github.com/labstack/echo/v4"
+
+	_ "github.com/mattn/go-sqlite3"
 )
+
+/*
+curl -X POST -d \
+    "{\"username\":\"siddaddy\",\"password\":\"pass\"}" \
+    http://127.0.0.1:1323/v1/adduser
+*/
+
+var users map[string]string
 
 func main() {
 	e := echo.New()
+
+	db, err := sql.Open("sqlite3", "./database/users.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	stmt := `CREATE TABLE userinfo(username TEXT, password TEXT);`
+	_, err = db.Exec(stmt)
+
+	// m := make(map[string]string)
+	// mptr := &m
 
 	// V1
 	version1_str := "/v1"
