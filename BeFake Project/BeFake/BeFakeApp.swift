af2c072fd264e6f0ca51e123257bbd360d3a28c1
Madhav Parthasarathy <madhavp2@illinois.edu>
2022-10-06 23:19:28 -0500

    added login page

---
+++ BeFake Project/BeFake/BeFakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeFakeApp.swift
+//  BeFake
+//
+//  Created by Madhav Parthasarathy on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct BeFakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
