43f2f7850ea538f3d7867ae992ce4dfcca291d64
Kelsi Padalia <kelsip2@illinois.edu>
2022-10-06 23:44:22 -0500

    Fix authuser API handler

--- src/server/apihandlers/authuser.go
+++ src/server/apihandlers/authuser.go
@@ -12,22 +12,22 @@ func Auth_user(c echo.Context) error {
 	db, err := sql.Open("sqlite3", "./database/users.db")
 
 	json_map := make(map[string]interface{})
-
-	username := json_map["username"].(string)
-	password := json_map["password"].(string)
-	err = json.NewDecoder(c.Request().Body).Decode(json_map)
+	err = json.NewDecoder(c.Request().Body).Decode(&json_map)
 	if err != nil {
 		return err
 	}
+
+	username := json_map["username"].(string)
+	password := json_map["password"].(string)
 
 	mainpass := ""
 	mainname := ""
 	row := db.QueryRow("SELECT username, password FROM userinfo WHERE username=?", username)
 	row.Scan(&mainname, &mainpass)
 
-	if mainpass != password {
-		return err
+	if (mainpass != password) {
+		return c.String(http.StatusBadRequest, "Authentication failed\n")
 	}
 
-	return c.String(http.StatusOK, mainname+" "+mainpass+" authenticated\n")
+	return c.String(http.StatusOK, "Authentication succeeded\n")
 }
