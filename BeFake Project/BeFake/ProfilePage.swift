f32cd8bd29ab07ca07fa6c9185aaec8b957f1e3e
mparthasarathy25 <87879274+mparthasarathy25@users.noreply.github.com>
2022-10-20 18:10:47 -0500

    Add files via upload

---
+++ BeFake Project/BeFake/ProfilePage.swift
@@ -0,0 +1,62 @@
+import SwiftUI
+
+struct ProfileView: View {
+    @State var username: String = ""
+    @State var password: String = ""
+    var body: some View {
+        VStack {
+            Image(systemName: "camera")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Profile")
+                .fontWeight(.heavy)
+                .foregroundColor(.black)
+                .font(Font.title)
+                .padding(.all,20.0)
+                .border(Color.yellow,width: 2)
+            Text("")
+                .fontWeight(.heavy)
+                .foregroundColor(.black)
+                .font(Font.title)
+                .padding(.top,20.0)
+            Text("")
+                .fontWeight(.heavy)
+                .foregroundColor(.black)
+                .font(Font.title)
+                .padding(.top,20.0)
+            Image(systemName: "circle")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Madhav Parthasarathy")
+                .fontWeight(.heavy)
+                .foregroundColor(.black)
+                .font(Font.title)
+                .padding(.top,20.0)
+            Text("@theparthasaurus")
+                .fontWeight(.heavy)
+                .foregroundColor(.black)
+                .padding(.all,20.0)
+            Text("Feed")
+                .fontWeight(.heavy)
+                .foregroundColor(.black)
+                .font(Font.title)
+                .padding(.all,20.0)
+            LazyVGrid(columns: [.init(.adaptive(minimum: 100, maximum: .infinity), spacing: 3)]) {
+                ForEach(1..<10) { ix in
+                    Image(systemName: "camera")
+                        .imageScale(.large)
+                        .foregroundColor(.accentColor)
+                        .padding(.bottom)
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+
+struct Content2View_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
