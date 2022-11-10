451456375103c2c0bda3260c3726a6c7a4851b65
Anagha Balaji <anaghabalaji@Anaghas-MacBook-Pro.local>
2022-11-10 16:13:16 -0600

    fixed buttons from text boxes

--- BeFake Project/BeFake/GroupsPage.swift
+++ BeFake Project/BeFake/GroupsPage.swift
@@ -45,40 +45,104 @@ struct Group3: View {
 }
 
 
-struct ExitGroup : View {
-    var body: some View {
+//struct ExitGroup : View {
+//    var body: some View {
+//
+//        VStack {
+//        //[…]
+//            Text("Exit Group")
+//                .font(.headline)
+//                .foregroundColor(.white)
+//                .padding()
+//                .frame(width: 150, height: 20)
+//                .background(Color.green)
+//                .cornerRadius(15.0)
+//        }
+//            .padding()
+//    }
+//}
 
+//struct ReportMember : View {
+//    var body: some View {
+//
+//        VStack {
+//        //[…]
+//            Text("Report Member")
+//                .font(.headline)
+//                .foregroundColor(.white)
+//                .padding()
+//                .frame(width: 150, height: 20)
+//                .background(Color.green)
+//                .cornerRadius(15.0)
+//        }
+//            .padding()
+//    }
+//}
+
+
+func whenClick() {
+    let session = URLSession.shared
+            let url = "http://127.0.0.1:1323/v1/adduser"
+            let request = NSMutableURLRequest(url: NSURL(string: url)! as URL)
+            request.httpMethod = "POST"
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            var params :[String: Any]?
+            params = ["username" : "kale-c", "password" : "gang"]
+            do{
+                request.httpBody = try JSONSerialization.data(withJSONObject: params, options: JSONSerialization.WritingOptions())
+                let task = session.dataTask(with: request as URLRequest as URLRequest, completionHandler: {(data, response, error) in
+                    if let response = response {
+                        let nsHTTPResponse = response as! HTTPURLResponse
+                        let statusCode = nsHTTPResponse.statusCode
+                        print ("status code = \(statusCode)")
+                    }
+                    if let error = error {
+                        print ("\(error)")
+                    }
+//                    if let data = data {
+//                        do{
+//                            let jsonResponse = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions())
+//                            print ("data = \(jsonResponse)")
+//                        }catch _ {
+//                            print ("OOps not good JSON formatted response")
+//                        }
+//                    }
+                })
+                task.resume()
+            }catch _ {
+                print ("Oops something happened buddy")
+            }
+}
+
+struct ExitGroup : View {
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    
+    var body: some View {
+        
         VStack {
         //[…]
-            Text("Exit Group")
-                .font(.headline)
-                .foregroundColor(.white)
-                .padding()
-                .frame(width: 150, height: 20)
-                .background(Color.green)
-                .cornerRadius(15.0)
+            Button(action: { whenClick() }){ Text( "Exit Group").padding().font(.largeTitle).fontWeight(.semibold).foregroundColor(.black)}
         }
             .padding()
     }
 }
 
 struct ReportMember : View {
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    
     var body: some View {
-
+        
         VStack {
         //[…]
-            Text("Report Member")
-                .font(.headline)
-                .foregroundColor(.white)
-                .padding()
-                .frame(width: 150, height: 20)
-                .background(Color.green)
-                .cornerRadius(15.0)
+            Button(action: { whenClick() }){ Text( "Report Member").padding().font(.largeTitle).fontWeight(.semibold).foregroundColor(.black)}
         }
             .padding()
     }
 }
-
 
 struct GroupsPage: View {
     @State var username: String = ""
