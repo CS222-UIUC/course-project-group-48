818cbf66bd58fea58ac28b3c0778176c35fe1ec5
Kelsi Padalia <kelsip2@illinois.edu>
2022-12-01 19:25:35 -0600

    wrote onClick method for each button

--- BeFake Project/BeFake/buttons.swift
+++ BeFake Project/BeFake/buttons.swift
@@ -111,34 +111,6 @@ func onClick_leaveGroup() {
 }
 
 
-func onClick_leaveGroup() {
-    let session = URLSession.shared
-            let url = "http://127.0.0.1:1323/v1/leavegroup"
-            let request = NSMutableURLRequest(url: NSURL(string: url)! as URL)
-            request.httpMethod = "POST"
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            var params :[String: Any]?
-            params = ["username" : "kale-c", "groupname" : "kelsi's group"]
-            do{
-                request.httpBody = try JSONSerialization.data(withJSONObject: params, options: JSONSerialization.WritingOptions())
-                let task = session.dataTask(with: request as URLRequest as URLRequest, completionHandler: {(data, response, error) in
-                    if let response = response {
-                        let nsHTTPResponse = response as! HTTPURLResponse
-                        let statusCode = nsHTTPResponse.statusCode
-                        print ("status code = \(statusCode)")
-                    }
-                    if let error = error {
-                        print ("\(error)")
-                    }
-                })
-                task.resume()
-            }catch _ {
-                print ("Oops something happened buddy")
-            }
-}
-
-
-
 func onClick_joinGroup() {
     let session = URLSession.shared
             let url = "http://127.0.0.1:1323/v1/joingroup"
